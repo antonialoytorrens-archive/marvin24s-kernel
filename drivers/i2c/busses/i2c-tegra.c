814f1e3b8e433e4293a25842d00f0b09a4c85435
Marc Dietrich <marvin24@gmx.de>
2011-05-12 21:32:56 +0200

    add more i2c functions

--- drivers/i2c/busses/i2c-tegra.c
+++ drivers/i2c/busses/i2c-tegra.c
@@ -533,7 +533,7 @@ static u32 tegra_i2c_func(struct i2c_adapter *adap)
 {
 	/* FIXME: For now keep it simple and don't support protocol mangling
 	   features */
-	return I2C_FUNC_I2C;
+	return I2C_FUNC_I2C | I2C_FUNC_SMBUS_EMUL;
 }
 
 static const struct i2c_algorithm tegra_i2c_algo = {
