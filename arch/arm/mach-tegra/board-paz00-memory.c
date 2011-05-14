b84cd1b2b26e0dc110dee144cf6cdb4e2003d66a
Marc Dietrich <marvin24@gmx.de>
2011-05-14 12:53:44 +0200

    add some more emc timings and also some more clocks.
    paz00 has at least two sdram configurations:
    	- Micron (MT47H128M8CF-25:H)
    	- Hynix (HY5PS1G831CLFP-Y5)
    
    The Hynix timings I got from source. The Micron timings are from
    my bct (but only for 333 Mhz I think). Some guesses for 166 MHz
    anyone ?

--- arch/arm/mach-tegra/board-paz00-memory.c
+++ arch/arm/mach-tegra/board-paz00-memory.c
@@ -23,7 +23,6 @@ struct tegra_board_emc_table {
 static const struct tegra_emc_table paz00_emc_tables_Hynix_333Mhz[] =
 {
 	{
-#if 0
 		.rate = 166500,   /* SDRAM frequency */
 		.regs = {
 			0x0000000a,   /* RC */
@@ -74,7 +73,6 @@ static const struct tegra_emc_table paz00_emc_tables_Hynix_333Mhz[] =
 			0x00000000,   /* CFG_CLKTRIM_2 */
 		}
 	}, {
-#endif
 		.rate = 333000,   /* SDRAM frequency */
 		.regs = {
 			0x00000018,   /* RC */
@@ -127,11 +125,127 @@ static const struct tegra_emc_table paz00_emc_tables_Hynix_333Mhz[] =
 	}
 };
 
-struct tegra_board_emc_table paz00_emc =
+static const struct tegra_emc_table paz00_emc_tables_Micron_333Mhz[] =
 {
-	.table 		= paz00_emc_tables_Hynix_333Mhz,
-	.table_size	= ARRAY_SIZE(paz00_emc_tables_Hynix_333Mhz),
-	.name		= "Hynix 333MHz",
+#if 0
+
+*** FIXME ! ***
+
+		.rate = 166500,   /* SDRAM frequency */
+		.regs = {
+			0x0000000a,   /* RC */
+			0x00000016,   /* RFC */
+			0x00000008,   /* RAS */
+			0x00000003,   /* RP */
+			0x00000004,   /* R2W */
+			0x00000004,   /* W2R */
+			0x00000002,   /* R2P */
+			0x0000000c,   /* W2P */
+			0x00000003,   /* RD_RCD */
+			0x00000003,   /* WR_RCD */
+			0x00000002,   /* RRD */
+			0x00000001,   /* REXT */
+			0x00000004,   /* WDV */
+			0x00000005,   /* QUSE */
+			0x00000004,   /* QRST */
+			0x00000009,   /* QSAFE */
+			0x0000000d,   /* RDV */
+			0x000004df,   /* REFRESH */
+			0x00000000,   /* BURST_REFRESH_NUM */
+			0x00000003,   /* PDEX2WR */
+			0x00000003,   /* PDEX2RD */
+			0x00000003,   /* PCHG2PDEN */
+			0x00000003,   /* ACT2PDEN */
+			0x00000001,   /* AR2PDEN */
+			0x0000000a,   /* RW2PDEN */
+			0x000000c8,   /* TXSR */
+			0x00000003,   /* TCKE */
+			0x00000006,   /* TFAW */
+			0x00000004,   /* TRPAB */
+			0x00000008,   /* TCLKSTABLE */
+			0x00000002,   /* TCLKSTOP */
+			0x00000000,   /* TREFBW */
+			0x00000000,   /* QUSE_EXTRA */
+			0x00000002,   /* FBIO_CFG6 */
+			0x00000000,   /* ODT_WRITE */
+			0x00000000,   /* ODT_READ */
+			0x00000083,   /* FBIO_CFG5 */
+			0xe03b0323,   /* CFG_DIG_DLL */
+			0x007fe010,   /* DLL_XFORM_DQS */
+			0x00001414,   /* DLL_XFORM_QUSE */
+			0x00000000,   /* ZCAL_REF_CNT */
+			0x00000000,   /* ZCAL_WAIT_CNT */
+			0x00000000,   /* AUTO_CAL_INTERVAL */
+			0x00000000,   /* CFG_CLKTRIM_0 */
+			0x00000000,   /* CFG_CLKTRIM_1 */
+			0x00000000,   /* CFG_CLKTRIM_2 */
+		}
+	},
+#endif
+	{
+		.rate = 333000,   /* SDRAM frequency */
+		.regs = {
+			0x00000014,   /* RC */
+			0x0000002b,   /* RFC */
+			0x0000000f,   /* RAS */
+			0x00000005,   /* RP */
+			0x00000004,   /* R2W */
+			0x00000005,   /* W2R */
+			0x00000004,   /* R2P */
+			0x0000000c,   /* W2P */
+			0x00000005,   /* RD_RCD */
+			0x00000005,   /* WR_RCD */
+			0x00000003,   /* RRD */
+			0x00000001,   /* REXT */
+			0x00000004,   /* WDV */
+			0x00000005,   /* QUSE */
+			0x00000004,   /* QRST */
+			0x00000009,   /* QSAFE */
+			0x0000000d,   /* RDV */
+			0x000009ff,   /* REFRESH */
+			0x00000000,   /* BURST_REFRESH_NUM */
+			0x00000003,   /* PDEX2WR */
+			0x00000003,   /* PDEX2RD */
+			0x00000005,   /* PCHG2PDEN */
+			0x00000005,   /* ACT2PDEN */
+			0x00000001,   /* AR2PDEN */
+			0x0000000f,   /* RW2PDEN */
+			0x000000c8,   /* TXSR */
+			0x00000003,   /* TCKE */
+			0x0000000c,   /* TFAW */
+			0x00000006,   /* TRPAB */
+			0x00000008,   /* TCLKSTABLE */
+			0x00000002,   /* TCLKSTOP */
+			0x00000000,   /* TREFBW */
+			0x00000000,   /* QUSE_EXTRA */
+			0x00000002,   /* FBIO_CFG6 */
+			0x00000000,   /* ODT_WRITE */
+			0x00000000,   /* ODT_READ */
+			0x00000083,   /* FBIO_CFG5 */
+			0x00000016,   /* CFG_DIG_DLL */
+			0x00000010,   /* DLL_XFORM_DQS */
+			0x00000008,   /* DLL_XFORM_QUSE */
+			0x00000000,   /* ZCAL_REF_CNT */
+			0x00000000,   /* ZCAL_WAIT_CNT */
+			0x00000000,   /* AUTO_CAL_INTERVAL */
+			0x00000000,   /* CFG_CLKTRIM_0 */
+			0x00000000,   /* CFG_CLKTRIM_1 */
+			0x00000000,   /* CFG_CLKTRIM_2 */
+		}
+	}
+};
+
+struct tegra_board_emc_table paz00_emc[] = {
+	{
+		.table 		= paz00_emc_tables_Hynix_333Mhz,
+		.table_size	= ARRAY_SIZE(paz00_emc_tables_Hynix_333Mhz),
+		.name		= "Hynix 333MHz",
+	},
+	{
+		.table 		= paz00_emc_tables_Micron_333Mhz,
+		.table_size	= ARRAY_SIZE(paz00_emc_tables_Micron_333Mhz),
+		.name		= "Micron 333MHz",
+	},
 };
 
 #define STRAP_OPT 0x008
@@ -152,6 +266,6 @@ void __init paz00_emc_init(void)
 	
 	pr_warning("EMC table: ramd_id: %d, tegra_sku_id %d\n", ram_id, tegra_sku_id);
 
-//	tegra_init_emc(NULL, 0);
-	tegra_init_emc(paz00_emc.table, paz00_emc.table_size);
+//egra_init_emc(NULL, 0);
+	tegra_init_emc(paz00_emc[1].table, paz00_emc[1].table_size);
 }
