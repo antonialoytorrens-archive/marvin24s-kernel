87ccb1764d9f992bbd01b100a7052387909c91d8
Marc Dietrich <marvin24@gmx.de>
2011-06-30 18:36:26 +0200

    add back lp0
    not used yet, but removes an error message from the boot log if
    lp0_vec is specified ;-)

--- arch/arm/mach-tegra/board-paz00.c
+++ arch/arm/mach-tegra/board-paz00.c
@@ -357,7 +357,7 @@ static struct tegra_suspend_platform_data paz00_suspend = {
 	.separate_req = true,
 	.corereq_high = false,
 	.sysclkreq_high = true,
-	.suspend_mode = TEGRA_SUSPEND_LP1,
+	.suspend_mode = TEGRA_SUSPEND_LP0,
 };
 
 static void __init tegra_paz00_init(void)
