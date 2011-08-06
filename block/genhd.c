a2429e54831c2dae65c125bc046f5883c0f6a7b7
Marc Dietrich <marvin24@gmx.de>
2011-08-06 14:32:49 +0200

    export block_class symbol needed by dm subsys

--- block/genhd.c
+++ block/genhd.c
@@ -1108,6 +1108,7 @@ static void disk_release(struct device *dev)
 struct class block_class = {
 	.name		= "block",
 };
+EXPORT_SYMBOL(block_class);
 
 static char *block_devnode(struct device *dev, mode_t *mode)
 {
