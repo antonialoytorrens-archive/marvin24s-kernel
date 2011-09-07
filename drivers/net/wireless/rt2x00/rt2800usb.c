05490ecb745e7df0c314b18e2e23fda8a56e66a0
Helmut Schaa <helmut.schaa@googlemail.com>
2011-09-07 20:10:02 +0200

    rt2x00: Minor optimizazion in txdone path
    We can save an indirect function call + some instructions for fetching
    the actual function pointer by passing the driver specific txwi pointer
    directly from rt2800pci/rt2800usb to rt2800lib instead of using the
    rt2800_drv_get_txwi callback.
    
    Signed-off-by: Helmut Schaa <helmut.schaa@googlemail.com>
    Acked-by: Gertjan van Wingerde <gwingerde@gmail.com>
    Signed-off-by: Ivo van Doorn <IvDoorn@gmail.com>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>
    
    Conflicts:
    
    	drivers/net/wireless/rt2x00/rt2800usb.c

--- drivers/net/wireless/rt2x00/rt2800usb.c
+++ drivers/net/wireless/rt2x00/rt2800usb.c
@@ -542,7 +542,8 @@ static void rt2800usb_txdone(struct rt2x00_dev *rt2x00dev)
 		}
 
 		if (entry)
-			rt2800_txdone_entry(entry, reg);
+			rt2800_txdone_entry(entry, reg,
+					    rt2800usb_get_txwi(entry));
 	}
 }
 
