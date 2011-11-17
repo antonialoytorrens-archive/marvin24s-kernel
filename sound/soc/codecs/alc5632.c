344bbdfefd2620fe573a379417453a515e5cf51c
Leon Romanovsky <leon@leon.nu>
2011-11-17 12:01:30 +0200

    ASoC: alc5632: Update of i2c_probe function to use regmap API only
    Signed-off-by: Leon Romanovsky <leon@leon.nu>
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    
    Conflicts:
    
    	sound/soc/codecs/alc5632.c

--- sound/soc/codecs/alc5632.c
+++ sound/soc/codecs/alc5632.c
@@ -1131,28 +1131,46 @@ static __devinit int alc5632_i2c_probe(struct i2c_client *client,
 				const struct i2c_device_id *id)
 {
 	struct alc5632_priv *alc5632;
-	int ret, vid1, vid2;
+	int ret, ret1, ret2;
+	unsigned int vid1, vid2;
 
 	alc5632 = devm_kzalloc(&client->dev,
 			 sizeof(struct alc5632_priv), GFP_KERNEL);
 	if (alc5632 == NULL)
 		return -ENOMEM;
 
-	vid1 = i2c_smbus_read_word_data(client, ALC5632_VENDOR_ID1);
-	vid2 = i2c_smbus_read_word_data(client, ALC5632_VENDOR_ID2);
-	if (vid1 < 0 || vid2 < 0) {
+	i2c_set_clientdata(client, alc5632);
+
+	alc5632->regmap = regmap_init_i2c(client, &alc5632_regmap);
+	if (IS_ERR(alc5632->regmap)) {
+		ret = PTR_ERR(alc5632->regmap);
+		dev_err(&client->dev, "regmap_init() failed: %d\n", ret);
+		return ret;
+	}
+
+	ret1 = regmap_read(alc5632->regmap, ALC5632_VENDOR_ID1, &vid1);
+	ret2 = regmap_read(alc5632->regmap, ALC5632_VENDOR_ID2, &vid2);
+	if (ret1 != 0 || ret2 != 0) {
 		dev_err(&client->dev,
-		"Failed to read chip ID: vid1=%d, vid2=%d\n", vid1, vid2);
+		"Failed to read chip ID: ret1=%d, ret2=%d\n", ret1, ret2);
+		regmap_exit(alc5632->regmap);
 		return -EIO;
 	}
 
-	vid1 = ((vid1 & 0xff) << 8) | (vid1 >> 8);
-	vid2 = (vid2 & 0xff);
+	vid2 >>= 8;
 
 	if ((vid1 != 0x10EC) || (vid2 != id->driver_data)) {
 		dev_err(&client->dev,
 		"Device is not a ALC5632: VID1=0x%x, VID2=0x%x\n", vid1, vid2);
+		regmap_exit(alc5632->regmap);
 		return -EINVAL;
+	}
+
+	ret = alc5632_reset(alc5632->regmap);
+	if (ret < 0) {
+		dev_err(&client->dev, "Failed to issue reset\n");
+		regmap_exit(alc5632->regmap);
+		return ret;
 	}
 
 	alc5632->id = vid2;
@@ -1164,23 +1182,7 @@ static __devinit int alc5632_i2c_probe(struct i2c_client *client,
 		return -EINVAL;
 	}
 
-	i2c_set_clientdata(client, alc5632);
-
-	alc5632->regmap = regmap_init_i2c(client, &alc5632_regmap);
-	if (IS_ERR(alc5632->regmap)) {
-		ret = PTR_ERR(alc5632->regmap);
-		dev_err(&client->dev, "regmap_init() failed: %d\n", ret);
-		return ret;
-	}
-
-	ret = alc5632_reset(alc5632->regmap);
-	if (ret < 0) {
-		dev_err(&client->dev, "Failed to issue reset\n");
-		regmap_exit(alc5632->regmap);
-		return ret;
-	}
-
-	ret =  snd_soc_register_codec(&client->dev,
+	ret = snd_soc_register_codec(&client->dev,
 		&soc_codec_device_alc5632, &alc5632_dai, 1);
 
 	if (ret < 0) {
