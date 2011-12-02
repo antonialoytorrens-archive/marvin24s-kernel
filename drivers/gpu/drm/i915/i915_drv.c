23292e31164087544c6bf9166662237cb89875c8
Stéphane Marchesin <marcheu@chromium.org>
2011-12-01 20:05:08 -0800

    CHROMIUM: drm/i915: Enable semaphores on SNB/IVB GPUs.
    Using the semaphores for synchronization should be more stable and faster
    than the current wait-based solution.
    
    Signed-off-by: Stéphane Marchesin <marcheu@chromium.org>
    
    BUG=chrome-os-partner:5678,chrome-os-partner:6590,chrome-os-partner:6588,chrome-os-partner:6294
    TEST=Build and run Chrome OS on stumpy, look for graphics glitches.
    
    Change-Id: I2d8ccfd61c5a4e426e00eefb79c66852870ff67d
    Reviewed-on: https://gerrit.chromium.org/gerrit/12367
    Reviewed-by: Olof Johansson <olofj@chromium.org>
    Tested-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -58,10 +58,10 @@ module_param_named(powersave, i915_powersave, int, 0600);
 MODULE_PARM_DESC(powersave,
 		"Enable powersavings, fbc, downclocking, etc. (default: true)");
 
-int i915_semaphores __read_mostly = -1;
+int i915_semaphores __read_mostly = 1;
 module_param_named(semaphores, i915_semaphores, int, 0600);
 MODULE_PARM_DESC(semaphores,
-		"Use semaphores for inter-ring sync (default: -1 (use per-chip defaults))");
+		"Use semaphores for inter-ring sync (default: true)");
 
 int i915_enable_rc6 __read_mostly = 1;
 module_param_named(i915_enable_rc6, i915_enable_rc6, int, 0600);
