31a840221511e22339bf167494dbb5e953e55984
Marc Dietrich <marvin24@gmx.de>
2012-02-18 16:41:22 +0100

    ARM: tegra: paz00: add usb suspend quirk
    Do not power down ehci3 on bus suspend (fixes hotplugging).
    
    Signed-off-by: Marc Dietrich <marvin24@gmx.de>

--- arch/arm/mach-tegra/board-paz00.c
+++ arch/arm/mach-tegra/board-paz00.c
@@ -31,6 +31,7 @@
 #include <linux/mfd/core.h>
 #include <linux/gpio.h>
 #include <linux/rfkill-gpio.h>
+#include <linux/platform_data/tegra_usb.h>
 #include <linux/platform_data/tegra_alc5632.h>
 
 #include <asm/mach-types.h>
@@ -303,6 +304,11 @@ static void paz00_i2c_init(void)
 
 static void paz00_usb_init(void)
 {
+	struct tegra_ehci_platform_data *pdata;
+
+	pdata = tegra_ehci3_device.dev.platform_data;
+	pdata->power_down_on_bus_suspend = 0;
+
 	platform_device_register(&tegra_ehci2_device);
 	platform_device_register(&tegra_ehci3_device);
 }
