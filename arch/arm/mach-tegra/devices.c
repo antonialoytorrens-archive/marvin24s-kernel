mmontuori/kernel-tegra-google

[ARM] tegra: add aes to devices.c
Change-Id: Id13075009d785e784ae6bd0feb9b29f3fa7184df
Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>

arch/arm/mach-tegra/devices.c
@@ -787,3 +787,24 @@ struct platform_device tegra_grhost_device = {
 	.resource = tegra_grhost_resources,
 	.num_resources = ARRAY_SIZE(tegra_grhost_resources),
 };
+
+static struct resource aes_resource[] = {
+[0] = {
+	.start  = TEGRA_VDE_BASE,
+	.end    = TEGRA_VDE_BASE + TEGRA_VDE_SIZE - 1,
+	.flags  = IORESOURCE_MEM,
+	},
+};
+
+static u64 aes_dma_mask = DMA_BIT_MASK(32);
+
+struct platform_device tegra_aes_device = {
+	.name = "tegra-aes",
+	.id = -1,
+	.resource = aes_resource,
+	.num_resources = ARRAY_SIZE(aes_resource),
+	.dev = {
+		.dma_mask = &aes_dma_mask,
+		.coherent_dma_mask = DMA_BIT_MASK(32),
+	},
+};

antonialoytorrens-archive/marvin24s-kernel

Conflicts:
	arch/arm/mach-tegra/devices.h

arch/arm/mach-tegra/devices.c
@@ -845,3 +845,24 @@ struct platform_device tegra_pwfm2_device = {
 	.num_resources  = 1,
 	.resource       = &tegra_pwfm2_resource,
 };
+
+static struct resource tegra_aes_resources[] = {
+	{
+		.start	= TEGRA_VDE_BASE,
+		.end	= TEGRA_VDE_BASE + TEGRA_VDE_SIZE - 1,
+		.flags	= IORESOURCE_MEM,
+	},
+};
+
+static u64 tegra_aes_dma_mask = DMA_BIT_MASK(32);
+
+struct platform_device tegra_aes_device = {
+	.name		= "tegra-aes",
+	.id		= -1,
+	.resource	= tegra_aes_resources,
+	.num_resources	= ARRAY_SIZE(tegra_aes_resources),
+	.dev	= {
+		.dma_mask = &tegra_aes_dma_mask,
+		.coherent_dma_mask = DMA_BIT_MASK(32),
+	},
+};