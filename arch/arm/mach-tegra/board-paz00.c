f1bc01e50049e2fb2702cc4f73c1c20adae9b171
Marc Dietrich <marvin24@gmx.de>
2012-02-26 19:10:09 +0100

    ARM: tegra: paz00: add i2c platform data
    Signed-off-by: Marc Dietrich <marvin24@gmx.de>

--- arch/arm/mach-tegra/board-paz00.c
+++ arch/arm/mach-tegra/board-paz00.c
@@ -29,6 +29,7 @@
 #include <linux/io.h>
 #include <linux/input.h>
 #include <linux/i2c.h>
+#include <linux/i2c-tegra.h>
 #include <linux/rfkill-gpio.h>
 
 #include <asm/mach-types.h>
@@ -168,6 +169,38 @@ static struct tegra_suspend_platform_data paz00_suspend_data = {
 	.board_resume = paz00_board_resume,
 };
 
+static struct tegra_i2c_platform_data paz00_i2c1_platform_data = {
+	.adapter_nr	= 0,
+	.bus_count	= 1,
+	.bus_clk_rate	= { 400000, 0 },
+};
+
+static const struct tegra_pingroup_config i2c2_ddc = {
+	.pingroup	= TEGRA_PINGROUP_DDC,
+	.func		= TEGRA_MUX_I2C2,
+};
+
+static const struct tegra_pingroup_config i2c2_gen2 = {
+	.pingroup	= TEGRA_PINGROUP_PTA,
+	.func		= TEGRA_MUX_I2C2,
+};
+
+static struct tegra_i2c_platform_data paz00_i2c2_platform_data = {
+	.adapter_nr	= 1,
+	.bus_count	= 2,
+	.bus_clk_rate	= { 100000, 100000 },
+	.bus_mux	= { &i2c2_ddc, &i2c2_gen2 },
+	.bus_mux_len	= { 1, 1 },
+};
+
+static struct tegra_i2c_platform_data paz00_dvc_platform_data = {
+	.adapter_nr	= 4,
+	.bus_count	= 1,
+	.bus_clk_rate	= { 400000, 0 },
+	.is_dvc		= true,
+};
+
+
 static struct platform_device *paz00_devices[] __initdata = {
 	&debug_uart,
 	&tegra_sdhci_device4,
@@ -175,10 +208,15 @@ static struct platform_device *paz00_devices[] __initdata = {
 	&wifi_rfkill_device,
 	&leds_gpio,
 	&gpio_keys_device,
+	&tegra_gart_device,
 };
 
 static void paz00_i2c_init(void)
 {
+	tegra_i2c_device1.dev.platform_data = &paz00_i2c1_platform_data;
+	tegra_i2c_device2.dev.platform_data = &paz00_i2c2_platform_data;
+	tegra_i2c_device4.dev.platform_data = &paz00_dvc_platform_data;
+
 	platform_device_register(&tegra_i2c_device1);
 	platform_device_register(&tegra_i2c_device2);
 	platform_device_register(&tegra_i2c_device4);
