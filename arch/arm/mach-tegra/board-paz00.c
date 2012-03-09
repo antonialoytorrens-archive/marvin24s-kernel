03c5910ed9752bf53f28a10aee901ae797bf9c99
Marc Dietrich <marvin24@gmx.de>
2012-03-09 17:32:09 +0100

    staging: nvec: request i2c-fast clock for the nvec
    This seems to be required by nvidia tree. Otherwise clk_get_sys
    will not find it.
    
    Signed-off-by: Marc Dietrich <marvin24@gmx.de>

--- arch/arm/mach-tegra/board-paz00.c
+++ arch/arm/mach-tegra/board-paz00.c
@@ -297,15 +297,17 @@ static void __init tegra_paz00_fixup(struct machine_desc *desc,
 
 static __initdata struct tegra_clk_init_table paz00_clk_init_table[] = {
 	/* name		parent		rate		enabled */
-	{ "uarta",	"pll_p",	216000000,	true },
-	{ "uartc",	"pll_p",	216000000,	true },
+	{ "uarta",	"pll_p",	216000000,	true  },
+	{ "uartc",	"pll_p",	216000000,	true  },
 
-	{ "pll_p_out4",	"pll_p",	24000000,	true },
+	{ "pll_p_out4",	"pll_p",	24000000,	true  },
 	{ "usbd",	"clk_m",	12000000,	false },
 	{ "usb2",	"clk_m",	12000000,	false },
 	{ "usb3",	"clk_m",	12000000,	false },
 
-	{ NULL,		NULL,		0,		0},
+	{ "i2c3",	"pll_p",	3200000,	true  },
+
+	{ NULL,		NULL,		0,		0     },
 };
 
 static struct tegra_sdhci_platform_data sdhci_pdata1 = {
