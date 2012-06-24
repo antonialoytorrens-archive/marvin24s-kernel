c8f35cff15f07706f193d8343d1da25550ad7550
Marc Dietrich <marvin24@gmx.de>
2012-06-24 22:02:20 +0200

    staging: nvec: add speaker mute/unmute gpio to paz00 board driver
    This allows toggling of the speaker gpio connected to the nvec.
    
    Signed-off-by: Marc Dietrich <marvin24@gmx.de>

--- drivers/staging/nvec/nvec_paz00.c
+++ drivers/staging/nvec/nvec_paz00.c
@@ -12,6 +12,7 @@
  */
 
 #include <linux/err.h>
+#include <linux/gpio.h>
 #include <linux/leds.h>
 #include <linux/module.h>
 #include <linux/slab.h>
@@ -31,6 +32,7 @@ enum nvec_oem0_ec_cmds {
 struct nvec_paz00_struct {
 	struct nvec_chip *nvec;
 	struct led_classdev *led_dev;
+	struct gpio_chip *gpio_dev;
 };
 
 struct nvec_paz00_struct nvec_paz00;
@@ -61,21 +63,71 @@ static int paz00_init_leds(struct device *dev)
 	return led_classdev_register(dev, nvec_paz00.led_dev);
 }
 
+static void nvec_gpio_set(struct gpio_chip *chip, unsigned offset, int value)
+{
+	struct nvec_chip *nvec = nvec_paz00.nvec;
+	char buf[] = { NVEC_OEM0, EXEC_EC_CMD, 0x59, 0x94 | !!value };
+
+	dev_dbg(nvec->dev, "gpio %d set to value %d\n", offset, value);
+	nvec_write_async(nvec, buf, 4);
+}
+
+static struct gpio_chip gpio_ops = {
+	.label		= "nvec",
+	.owner		= THIS_MODULE,
+	.set		= nvec_gpio_set,
+	.base		= -1,
+	.ngpio		= 1,
+	.can_sleep	= 1,
+};
+
+static int paz00_init_gpios(struct device *dev)
+{
+	struct device_node *cell, *np = dev->parent->of_node;
+
+	if (!np) {
+		dev_err(dev, "no of node found\n");
+		return -ENODEV;
+	}
+
+	cell = of_find_node_by_name(np, "cells");
+	if (!cell) {
+		dev_err(dev, "no cell info found\n");
+		return -ENODEV;
+	}
+
+	cell = of_find_node_by_name(cell, "paz00");
+	if (!cell) {
+		dev_err(dev, "no platform data found\n");
+		return -ENODEV;
+	}
+
+	gpio_ops.of_node = cell;
+	nvec_paz00.gpio_dev = &gpio_ops;
+	nvec_paz00.gpio_dev->dev = dev;
+
+	return gpiochip_add(nvec_paz00.gpio_dev);
+}
+
 static int nvec_paz00_probe(struct platform_device *pdev)
 {
 	struct nvec_chip *nvec = dev_get_drvdata(pdev->dev.parent);
+	int ret;
 
 	platform_set_drvdata(pdev, &nvec_paz00);
 	nvec_paz00.nvec = nvec;
 
-	return paz00_init_leds(&pdev->dev);
+	ret = paz00_init_leds(&pdev->dev);
+	if (!ret)
+		ret = paz00_init_gpios(&pdev->dev);
+
+	return ret;
 }
 
 static int nvec_paz00_remove(struct platform_device *pdev)
 {
 	led_classdev_unregister(nvec_paz00.led_dev);
-
-	return 0;
+	return gpiochip_remove(nvec_paz00.gpio_dev);
 }
 
 static struct platform_driver nvec_paz00_driver = {
