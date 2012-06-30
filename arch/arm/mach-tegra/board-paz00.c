56eb7e04c0bd71871eaed0691db307d7e78258cc
Marc Dietrich <marvin24@gmx.de>
2012-06-30 14:13:41 +0200

    ARM: tegra: paz00: disable power on suspend on ehci3
    Should fix hotplug on resume.
    
    Signed-off-by: Marc Dietrich <marvin24@gmx.de>

--- arch/arm/mach-tegra/board-paz00.c
+++ arch/arm/mach-tegra/board-paz00.c
@@ -222,8 +222,7 @@ static struct tegra_ehci_platform_data tegra_ehci_pdata[] = {
 	[2] = {
 			.phy_config = &utmi_phy_config[1],
 			.operating_mode = TEGRA_USB_HOST,
-			.power_down_on_bus_suspend = 1,
-			.hotplug = 1,
+			.power_down_on_bus_suspend = 0,
 			.default_enable = true,
 	},
 };
