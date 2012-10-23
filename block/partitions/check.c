602b7704e23c5ba891af9b32209a7e242d378f69
Marc Dietrich <marvin24@gmx.de>
2012-10-23 18:31:16 +0200

    block: add support for nvidia/tegra partitions
    This adds support for NVIDIA style parition table found on many
    Tegra android devices. Alternatively, it adds support for adding
    partitions on the kernel command line.
    
    Signed-off-by: Marc Dietrich <marvin24@gmx.de>

--- block/partitions/check.c
+++ block/partitions/check.c
@@ -19,6 +19,8 @@
 
 #include "check.h"
 
+#include "tegrapart.h"
+#include "nvtegra.h"
 #include "acorn.h"
 #include "amiga.h"
 #include "atari.h"
@@ -41,6 +43,12 @@ static int (*check_part[])(struct parsed_partitions *) = {
 	 * Probe partition formats with tables at disk address 0
 	 * that also have an ADFS boot block at 0xdc0.
 	 */
+#ifdef CONFIG_NVTEGRAPARTITION
+	nvtegra_partition,
+#endif
+#ifdef CONFIG_TEGRAPART_PARTITION
+	tegrapart_partition,
+#endif
 #ifdef CONFIG_ACORN_PARTITION_ICS
 	adfspart_check_ICS,
 #endif
