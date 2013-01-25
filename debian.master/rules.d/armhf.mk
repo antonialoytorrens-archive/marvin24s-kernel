58ca6c67790d93c29e09207de8ad26a6cc5cd2e2
Marc Dietrich <marvin24@gmx.de>
2013-01-25 22:49:06 +0100

    add tegra support
    Signed-off-by: Marc Dietrich <marvin24@gmx.de>

--- debian.master/rules.d/armhf.mk
+++ debian.master/rules.d/armhf.mk
@@ -3,7 +3,7 @@ build_arch	= arm
 header_arch	= arm
 defconfig	= defconfig
 #flavours	= omap highbank
-flavours	= omap
+flavours	= tegra
 build_image	= zImage
 kernel_file	= arch/$(build_arch)/boot/zImage
 install_file	= vmlinuz
@@ -15,3 +15,5 @@ do_tools	= false
 # Flavour specific configuration.
 #dtb_file_highbank	= arch/$(build_arch)/boot/highbank.dtb
 dtb_files_omap	= imx6q-sabrelite.dtb omap3-beagle-xm.dtb omap4-panda.dtb omap4-panda-es.dtb
+dtb_files_tegra = tegra20-harmony.dtb tegra20-paz00.dtb tegra20-seaboard.dtb tegra20-trimslice.dtb tegra20-whistler.dtb tegra30-cardhu-a04.dtb \
+		  tegra20-medcom-wide.dtb tegra20-plutux.dtb tegra20-tec.dtb tegra20-ventana.dtb tegra30-cardhu-a02.dtb
